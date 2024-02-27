4cce8387fde8ee81bbf702bc94dfa380a30f147a
G Sree Rohith Chowdary <sreerohit2004@gmail.com>
2024-02-27 08:29:19 +0530

    init fix

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,30 @@
+import cv2
+from tensorflow.keras.models import load_model
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(1)
+
+while True:
+
+    ret, img = cap.read()
+
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+    faces = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.3, minNeighbors=5, minSize=(30,30))
+
+    model = load_model('fed_50epoch.h5')
+
+    face_roi = gray_img
+    for (x, y, w, h) in faces:
+        face_roi = gray_img[y:y+h, x:x+w]
+
+    resized_face_img = cv2.resize(face_roi, (48,48))
+
+    model_input_img = resized_face_img.reshape((1,48,48,1))
+    model_input_img = model_input_img / 255.0
+
+    prediction = model.predict(model_input_img)
+
+    print(prediction)
